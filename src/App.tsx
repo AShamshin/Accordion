152318a04f063d6e7f92e18b6fc49eac38e16d30
AShamshin <albert.shamshin@mail.ru>
2023-03-16 20:14:28 +0300

    17 - React.memo

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,39 @@ import { OnOfCopy } from './components/OnOf/OnOf copy';
 import { Select } from './components/Select/Select';
 
 function App() {
+  const NewMessagesCounter = (props: { count: number }) => {
+    return <div>{props.count}</div>;
+  };
+  const UsersSecret = (props: { users: Array<string> }) => {
+    return (
+      <div>
+        {props.users.map((u, i) => (
+          <div key={i}>{u}</div>
+        ))}
+      </div>
+    );
+  };
+
+  const Users = React.memo(UsersSecret);
+
+  const Example1 = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem']);
+    return (
+      <>
+        <button
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button>
+        <NewMessagesCounter count={counter} />
+        <Users users={['Dimych', 'Valera', 'Artem']} />
+      </>
+    );
+  };
+
   let [ratingValue, setRatingValue] = useState<RatingValueType>(0);
   let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false);
   const [on, iiiOn] = useState<any>(false);
@@ -24,21 +57,16 @@ function App() {
     <div className='App'>
       {/* <OnOfCopy on={on} iiiOn={iiiOn} />
       {on.toString()} */}
-
       {/* <OnOf onChange={iiiOn} />
       {on.toString()} */}
-
       {/* <PageTitle title={'This is APP component'} />
       <PageTitle title={'My friends'} /> */}
-
       {/* <Rating value={ratingValue} onClick={setRatingValue} /> */}
-
       {/* <Accordion
         titleValue={'Menu'}
         onChange={() => setAccordionCollapsed(!accordionCollapsed)}
         collapsed={accordionCollapsed}
       /> */}
-
       {/* <Accordion
         titleValue={'Users'}
         collapsed={accordionCollapsed}
@@ -71,12 +99,10 @@ function App() {
           ]}
         /> */}
       </>
-
       {/* <UncontrolledAccordion titleValue={'Menu'} /> */}
-
-      <UncontrolledAccordion titleValue={'Users'} />
-
+      {/* <UncontrolledAccordion titleValue={'Users'} /> */}
       {/* <UncontrolledRating /> */}
+      <Example1 />
     </div>
   );
 }
